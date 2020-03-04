d4cf7135ac059fb99ac3233e21cfb10a88a529f3
Nolan Kingdon <nolankingdon@geotab.com>
2020-03-04 16:57:37 -0500

    Handling bad requests

--- test/node/nocks/nock.js
+++ test/node/nocks/nock.js
@@ -2,13 +2,26 @@ const nock = require('nock');
 const mocks = require('../../mocks/mocks');
 let authenticationAttempt = 0;
 
+// Decodingthe encoded request
+const parseBody = (bodyStr) => {
+    return JSON.parse(bodyStr['JSON-RPC']);
+}
+// const auth = nock(`https://${mocks.server}/apiv1/`)
+//             .persist()
+//             .filteringRequestBody( (body) => parseBody(body) )
+//             .post('/Authenticate')
+//             .reply(200, (uri, body) => parseBody(body))
+
 // Authentication
 // Alternating between credentials to help test the forget function
 const auth1 = nock(`https://${mocks.server}/apiv1/`, {
         conditionally: () => authenticationAttempt % 2 === 0
     })
         .persist()
-        .post('/Authenticate')
+        .post('/Authenticate', (body) => {
+            let parsed = parseBody(body);
+            return parsed.params.database === 'testDB';
+        })
         .reply(200, () => {
             authenticationAttempt++;
             return {result: mocks.credentials}
@@ -18,11 +31,30 @@ const auth2 = nock(`https://${mocks.server}/apiv1/`, {
             conditionally: () => authenticationAttempt % 2 === 1
         })
         .persist()
-        .post('/Authenticate')
+        .post('/Authenticate', (body) => {
+            let parsed = parseBody(body);
+            return parsed.params.database === 'testDB';
+        })
         .reply(200, () => {
             authenticationAttempt++;
             return {result: mocks.refreshedCredentials}
-        });
+        })
+
+const auth3 = nock(`https://${mocks.server}/apiv1/`)
+        .persist()
+        .post('/Authenticate', (body) => {
+            let parsed = parseBody(body);
+            return parsed.params.database === 'badinfo';
+        })
+        .reply(200, () => {
+            authenticationAttempt++;
+            return {
+                    error: {
+                        name: "InvalidUserException",
+                        message: "Bad info entered"
+                    }
+            }
+        })
 
 // Get requests
 const get = nock(`https://${mocks.server}/apiv1/`)
@@ -41,6 +73,15 @@ get
     })
     .reply(200, {result: mocks.user});
 
+// Failing call
+get
+    .post('/Geet')
+    .reply(200, {result: {
+        error: {
+            name: 'InvalidRequest',
+            message: 'Invalid request information entered'
+        }
+    }})
 // GetCountOf Requests
 const getCount = nock(`https://${mocks.server}/apiv1/`)
             .persist()
