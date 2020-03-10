b9fc4b417011b9487e62dfffbf1db7cdfdce01aa
Nolan Kingdon <nolankingdon@geotab.com>
2020-03-10 14:46:07 -0400

    Refactoring webpack config

--- webpack.production.js
+++ webpack.production.js
@@ -1,4 +1,3 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
@@ -36,10 +35,5 @@ module.exports = merge(common, {
                 },
             },
         ]
-    },
-    // plugins: [
-    //     new UglifyJsPlugin({
-    //         test: /\.js(\?.*)?$/i
-    //     }),
-    // ]
+    }
 });
