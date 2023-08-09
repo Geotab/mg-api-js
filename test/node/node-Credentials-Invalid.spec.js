0006d293a8ce67773736da876ca5d501a5c719e0
Brian Cho <wonjuncho@geotab.com>
2023-08-09 13:03:50 -0400

    added tests

--- test/node/node-Credentials-Invalid.spec.js
+++ test/node/node-Credentials-Invalid.spec.js
@@ -73,4 +73,54 @@ describe('User loads GeotabApi node module and triggers an error (Credentials)',
                             .catch( err => err );
         assert.isTrue(response.message.includes('MissingMethodException'), 'Promise response undefined');
     });
+
+    it('Api should gracefully handle a multicall failure (Callback)', async () => {
+        let login = mocks.login;
+        let api = new GeotabApi({
+                credentials: {
+                    database: login.credentials.database,
+                    userName: login.credentials.username,
+                    password: login.credentials.password
+                },
+                path: login.path,
+        }, {rememberMe: false});
+
+        let response = await new Promise((resolve, reject) => {
+            let calls = [
+                ["GetCountOff", { typeName: "Device" }],
+                ["GetCountOf", { typeName: "User" }]
+            ];
+            api.multiCall(calls, function(success){
+                resolve(success);
+            }, function(message, error){
+                reject(error);
+            });
+        })
+        .then( resolved => resolved )
+        .catch( error => error );
+
+        assert.equal(response.data.type, 'MissingMethodException');
+    });
+
+    it('Api should gracefully handle a multicall failure (Async)', async () => {
+        let login = mocks.login;
+        let api = new GeotabApi({
+            credentials: {
+                database: login.credentials.database,
+                userName: login.credentials.username,
+                password: login.credentials.password
+            },
+            path: login.path,
+        }, {rememberMe: false});
+
+        let calls = [
+            ["GetCountOff", { typeName: "Device" }],
+            ["GetCountOf", { typeName: "User" }]
+        ];
+        let call = api.multiCall(calls);
+        let response = await call
+                            .then( result => result )
+                            .catch( err => err );
+        assert.isTrue(response.message.includes('MissingMethodException'), 'Promise response undefined');
+    });
 });
